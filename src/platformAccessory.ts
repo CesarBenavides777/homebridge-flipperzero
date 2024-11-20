c3bdf9ed757afa0fc0449eef48b43f1d9ee7625a
Cesar Benavides <cesarbenavides@icloud.com>
2024-11-20 09:02:08 -0600

    setting up publish

--- src/platformAccessory.ts
+++ src/platformAccessory.ts
@@ -1,21 +1,21 @@
-import { Service, Characteristic, CharacteristicValue } from "hap-nodejs";
-import { exec } from "child_process";
-import { FlipperZeroPlatform } from "./platform.js";
-import { PlatformAccessory } from "homebridge";
+import { Service, Characteristic, CharacteristicValue } from 'hap-nodejs';
+import { exec } from 'child_process';
+import { FlipperZeroPlatform } from './platform.js';
+import { PlatformAccessory } from 'homebridge';
 
 export class FlipperZeroAccessory {
   private service: Service;
 
   constructor(
     private readonly platform: FlipperZeroPlatform,
-    private readonly accessory: PlatformAccessory
+    private readonly accessory: PlatformAccessory,
   ) {
     const device = accessory.context.device;
 
     this.accessory
       .getService(Service.AccessoryInformation)!
-      .setCharacteristic(Characteristic.Manufacturer, "Flipper Devices")
-      .setCharacteristic(Characteristic.Model, "Flipper Zero")
+      .setCharacteristic(Characteristic.Manufacturer, 'Flipper Devices')
+      .setCharacteristic(Characteristic.Model, 'Flipper Zero')
       .setCharacteristic(Characteristic.SerialNumber, device.uniqueId);
 
     this.service =
@@ -26,14 +26,19 @@ export class FlipperZeroAccessory {
 
     this.service
       .getCharacteristic(Characteristic.On)
-      .onSet(this.setCommand.bind(this, "onCommandFile"))
+      .onSet(this.setCommand.bind(this, 'onCommandFile'))
       .onGet(this.getStatus.bind(this));
   }
 
   async setCommand(commandFile: string, value: CharacteristicValue) {
     const device = this.accessory.context.device;
+    // log the value
+    this.platform.log.info(
+      `Setting ${device.displayName} to ${value ? 'On' : 'Off'}`,
+    );
 
     // Ensure that arguments with spaces are wrapped in quotes
+    // eslint-disable-next-line max-len
     const commandToRun = `python3 /Users/CESARMAC/desktop/env/homebridge-flipperzero/src/commands/script.py --send "${device.displayName}" --type ${device.signalType} --file ${device[commandFile]}`;
 
     exec(commandToRun, (error, stdout, stderr) => {
--- src/settings.ts
+++ src/settings.ts
@@ -1,2 +1,2 @@
-export const PLATFORM_NAME = "FlipperZeroPlatform"; // Must match config.json
-export const PLUGIN_NAME = "homebridge-flipperzero"; // Plugin registration name
+export const PLATFORM_NAME = 'FlipperZeroPlatform'; // Must match config.json
+export const PLUGIN_NAME = 'homebridge-flipperzero'; // Plugin registration name
